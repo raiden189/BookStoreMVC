d97bcf3c1932f0ac9a8c345fc71c422ed77c325a
raidenthor <raidengreat123@gmail.com>
2023-02-14 10:45:33 +0800

    Create mocking data service

---
+++ BookStoreMVC/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreMVC.Models
+{
+    public class Book
+    {
+        public string BookId { get; set; }
+
+        public string BookName { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+    }
+}
